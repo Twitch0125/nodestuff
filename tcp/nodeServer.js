02cb5cbd4ea61b6656900ed9d4598cdf7a52b9fd
kaleb <kaleb@pop-os.localdomain>
2019-05-21 10:40:29 -0600

    Fixed crash from client disconnect

--- tcp/nodeServer.js
+++ tcp/nodeServer.js
@@ -4,39 +4,38 @@ const fs = require("fs");
 let randomName = require("random-name");
 process.stdin.setEncoding("utf8");
 let clients = [];
-const serverStuff = fs.createWriteStream("server.log");
-
+const serverStuff = fs.createWriteStream("chat.log");
+//log function because I tired of writing serverStuff.write(data, "UTF8")
 function log(data) {
   serverStuff.write(data, "UTF8");
 }
-
+const encoding = "utf8";
 let server = net
   .createServer(client => {
-    client.write("Hello! Welcome to the chat room! ");
+    client.write("Hello! Welcome to the chat room! \n");
     let newId = uuidv4();
     let newName = randomName();
+    log(`new dude: ${newName}\n`);
     clients.forEach(currClient => {
-      currClient.client.write(`new dude: ${newName}`);
-      log(`new dude: ${newName}`);
+      currClient.client.write(`new dude: ${newName}`); //tell all other clients who the new person is
     });
     clients.push({ id: newId, client: client, name: newName });
-    client.write(`your id is: ${newId}\n your name is: ${newName}\n\n`);
+    client.write(`your id is: ${newId}\n`);
+    client.write(`your name is: ${newName}`);
     client.on("data", data => {
+      log(`${newName}: ${data}\n`);
       clients.forEach(currClient => {
+        //broadcast message to every user except the user that sent it
         if (currClient.client != client) {
           currClient.client.write(`${newName}: ${data}`);
-          log(`${newName}: ${data}`);
         }
       });
     });
-    client.on("close", (client, err) => {
-      if (err) {
-        return console.log("whoopsie");
-      }
-      clients = clients.filter(currClient => currClient.client != client);
+    client.on("close", () => {
+      log(`${newName} has fled the server\n`);
+      clients = clients.filter(currClient => currClient.client.writable); //remove any clients that are no longer available.
       clients.forEach(currClient => {
         currClient.client.write(`${newName} has fled the server`);
-        log(`${newName} has fled the server`);
       });
     });
   })
